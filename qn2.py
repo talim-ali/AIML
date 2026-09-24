524ff6de8f9ac5b4446480b83f2ba7d8540a6c40
Talim Ali <talim392004@gmail.com>
2026-09-24 12:21:09 +0530

    Add files via upload

---
+++ qn2.py
@@ -0,0 +1,7 @@
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+c=float(input("enter third number:"))
+a=float(a)
+b=float(b)
+avg=(a+b+c)/3
+print(f"average of three number is {avg}")
